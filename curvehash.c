7a75fef144aba245a463a51bbb1e71691d093231
trakds <trak-999@mail.ru>
2021-02-27 17:36:30 +0300

    Update curvehash.c

--- curvehash.c
+++ curvehash.c
@@ -215,7 +215,7 @@ void curve_hash(const char* input, char* output, uint32_t len)
         // Assume SHA256 result as private key and compute uncompressed public key
         //secp256k1_ec_pubkey_create(ctx, &pubkey, (unsigned char *) hash);
         //secp256k1_ec_pubkey_serialize(ctx, pub, &publen, &pubkey, SECP256K1_EC_UNCOMPRESSED);
-	secp256k1_ecdsa_pubkey_create(pub, publen, (unsigned char *) hash, false)
+	secp256k1_ecdsa_pubkey_create(pub, publen, (unsigned char *) hash, 0);
 
         // Use SHA256 to hash resulting public key
         sha256hash((unsigned char *) hash, pub, 65);
